dcad7cde8bde7521ec6c3b7d3d9b970db0a99c59
Andy Herbert <andyman1222@users.noreply.github.com>
2025-01-22 10:19:12 -0700

    fix loadFileAjax function location

--- engine/shaderProgram.js
+++ engine/shaderProgram.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import { loadFileAJAX } from "./common/helpers-and-types";
+
 //Wrapper for openGL shader program, working with a vertex and fragment shader
 export class ShaderProgram{
 
