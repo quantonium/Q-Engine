b8710683b27e49e4a1702aab42e42d36f68ff67a
Andy Herbert <andyman1222@users.noreply.github.com>
2025-01-26 23:07:25 -0700

    fix getPlane function

--- engine/common/helpers-and-types.js
+++ engine/common/helpers-and-types.js
@@ -276,11 +276,11 @@ export function getMidpoint(points) {
  * Returns a 4-vector representation of a plane- (x, y, z, b)
  * @param {*} points 3 points
  */
-export function getPlane(points, normFunction = normalize) {
+export function getPlane(points) {
 	if (points.length == 3) {
 		var cp = cross(subtract(points[2], points[0]),subtract(points[1], points[0]))
 		var d = dot(cp, points[2])
-		return normFunction(vec4(cp[0], cp[1], cp[2], d))
+		return vec4(cp[0], cp[1], cp[2], d)
 	}
 	throw "Can only get plane intersecting 3 points."
 }
